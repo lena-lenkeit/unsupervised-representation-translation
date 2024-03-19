4ede8c5e16730b4b08f35888a6cf1e29f9f5518f
lena-lenkeit <92172030+lena-lenkeit@users.noreply.github.com>
2024-03-19 15:22:58 +0100

    Updated colab packaging util

--- colab-package.py
+++ colab-package.py
@@ -6,6 +6,9 @@ if __name__ == "__main__":
 
     with tarfile.open(output_filename, "w:gz") as tar:
         tar.add("src")
+        tar.add("conf")
         tar.add("train-arae.py")
         tar.add("eval-arae.py")
+        tar.add("train-clm.py")
+        tar.add("eval-clm.py")
         tar.add("pyproject.toml")
