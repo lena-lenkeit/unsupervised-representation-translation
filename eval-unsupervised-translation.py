945389f1ef6cb1a3e3a132ad5a2785d29fcb0cee
lena-lenkeit <92172030+lena-lenkeit@users.noreply.github.com>
2024-04-03 11:47:43 +0200

    GenerationConfig for translation

--- eval-unsupervised-translation.py
+++ eval-unsupervised-translation.py
@@ -2,14 +2,19 @@ import functools
 
 import datasets
 import torch
-from transformers import T5ForConditionalGeneration, T5Tokenizer, TrainingArguments
+from transformers import (
+    GenerationConfig,
+    T5ForConditionalGeneration,
+    T5Tokenizer,
+    TrainingArguments,
+)
 
 from arae.datasets import make_wikisentence_dataset
 from arae.trainers import EncoderDecoderLMForUnsupervisedTranslationTrainer
 
 
 def main():
-    model_path = "results/flan-t5-small-encdecv2/checkpoint-500"
+    model_path = "results/flan-t5-small-encdecv2-encoderclassifier/checkpoint-1300"
     TokenizerType = T5Tokenizer
     ModelType = T5ForConditionalGeneration
 
@@ -22,13 +27,32 @@ def main():
     )
     assert isinstance(model, ModelType)
 
+    print(tokenizer.convert_tokens_to_ids("[LABEL_0]"))
+
+    # model.get_encoder().train()
+
     while True:
         text_input = input("Encoder Input Text: ")
 
         token_inputs = tokenizer(text_input, return_tensors="pt").to(model.device)
-        token_outputs = model.generate(**token_inputs)
+        # decoder_inputs = tokenizer("[LABEL_1]", return_tensors="pt").to(model.device)
+
+        config = GenerationConfig(
+            do_sample=True,
+            top_k=0,
+            top_p=0.95,
+        )
+        token_outputs = model.generate(
+            **token_inputs,
+            decoder_start_token_id=tokenizer.convert_tokens_to_ids("[LABEL_0]"),
+            # decoder_input_ids=decoder_inputs.input_ids[:, :1],
+            generation_config=config
+        )
         text_output = tokenizer.batch_decode(token_outputs)
 
+        print(token_inputs)
+        # print(decoder_inputs)
+        print(token_outputs)
         print(text_output)
 
 
